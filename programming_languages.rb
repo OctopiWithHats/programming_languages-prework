59dc68496093e2128742703fa3873d9cd7454384
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-22 22:11:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,7 @@
 def reformat_languages(languages)
-  # your code here	  new_hash = {} 
+  # your code here	  
+  
+  new_hash = {} 
 
    languages.each do |style, description|
     description.each do |lang, type|
